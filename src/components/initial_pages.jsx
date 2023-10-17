3bbb6b602c267240732b3dc59e9745fb2ce9f15b
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-10-17 20:55:09 -0300

    init_page

---
+++ src/components/initial_pages.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import AnaMaria from "../assets/anamaria.png"
+
+export const Init_page = () => {
+    return (
+        <div className='postulantes'>
+            <div className='postulante'>
+                <div className='imgPostu'>
+                    <img src={AnaMaria} alt='logo usm' />
+                </div>
+                <div className='info1'>
+                    <h3>Ana María de las Mercedes</h3>
+                    <h4>Postulante a ayudantia de Estructuras Discretas</h4>
+                </div>
+                <div className='info2g'>
+                    <div className='info2'>
+                        <div className='promedio'>
+                            <h4>Promedio</h4>
+                            <h4>78</h4>
+                        </div>
+                        <div className='VSA'>
+                            <h4>Veces Siendo ayudante</h4>
+                            <h4>2</h4>
+                        </div>
+                        <div className='TipoPost'>
+                            <h4>Postulante a</h4>
+                            <h4>Laboratorio</h4>
+                        </div>
+                    </div>
+                    <div className='buttons'>
+                        <button>Perfil academico</button>
+                        <button>Seleccionar</button>
+                    </div>
+                </div>
+            </div>
+            <div className='postulante'>
+                <div className='imgPostu'>
+                    <img src={AnaMaria} alt='logo usm' />
+                </div>
+                <div className='info1'>
+                    <h3>Ana María de las Mercedes</h3>
+                    <h4>Postulante a ayudantia de Estructuras Discretas</h4>
+                </div>
+                <div className='info2g'>
+                    <div className='info2'>
+                        <div className='promedio'>
+                            <h4>Promedio</h4>
+                            <h4>78</h4>
+                        </div>
+                        <div className='VSA'>
+                            <h4>Veces Siendo ayudante</h4>
+                            <h4>2</h4>
+                        </div>
+                        <div className='TipoPost'>
+                            <h4>Postulante a</h4>
+                            <h4>Laboratorio</h4>
+                        </div>
+                    </div>
+                    <div className='buttons'>
+                        <button>Perfil academico</button>
+                        <button>Seleccionar</button>
+                    </div>
+                </div>
+            </div>
+            <div className='postulante'>
+                <div className='imgPostu'>
+                    <img src={AnaMaria} alt='logo usm' />
+                </div>
+                <div className='info1'>
+                    <h3>Ana María de las Mercedes</h3>
+                    <h4>Postulante a ayudantia de Estructuras Discretas</h4>
+                </div>
+                <div className='info2g'>
+                    <div className='info2'>
+                        <div className='promedio'>
+                            <h4>Promedio</h4>
+                            <h4>78</h4>
+                        </div>
+                        <div className='VSA'>
+                            <h4>Veces Siendo ayudante</h4>
+                            <h4>2</h4>
+                        </div>
+                        <div className='TipoPost'>
+                            <h4>Postulante a</h4>
+                            <h4>Laboratorio</h4>
+                        </div>
+                    </div>
+                    <div className='buttons'>
+                        <button>Perfil academico</button>
+                        <button>Seleccionar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Init_page
