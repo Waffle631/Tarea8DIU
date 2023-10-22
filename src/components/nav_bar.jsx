4432c9bf0c29eb5ffd1e9ce050f2552eca7f0f6a
Gabriel Vergara <gabriel.vergaraa@usm.cl>
2023-10-22 11:10:26 -0300

    a

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 export const NavBar = () => {
   return (
     <nav className='nav-bar'>
+      <span className='logo-text'>SPA</span>
       <NavLink
         className={({ isActive }) => `nav-bar__link ${isActive ? 'nav-bar__link--active' : ''}`}
         to='/'
